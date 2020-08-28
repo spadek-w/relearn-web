9ba9f75e4ebbcb632e9b115473a8f6b15fac957f
spadekw <1711980439@qq.com>
2020-08-28 17:48:45 +0800

    es6 数据类型的扩展 (一)

---
+++ es6/扩展-string-Array-Object.js
@@ -0,0 +1,37 @@
+
+/**
+    字符串新增方法 
+    JavaScript 的字符串是不可变的（immutable），
+    String 类定义的方法都不能改变字符串的内容。
+    像 String.toUpperCase() 这样的方法，返回的是全新的字符串，而不是修改原始字符串
+ */
+/** 01.---------- */
+// includes()：返回布尔值，表示是否找到了参数字符串。
+// startsWith()：返回布尔值，表示参数字符串是否在原字符串的头部。
+// endsWith()：返回布尔值，表示参数字符串是否在原字符串的尾部。
+var str1 = "hello word";
+console.log(str1.includes('he'))
+/** 02.---------- */
+/**
+    repeat方法返回一个新字符串，表示将原字符串重复n次
+    replace 替换
+ */
+var str2 = str1.repeat(2)
+console.log(str2); 
+/** 03.---------- */
+//padStart()：头部补全
+//padEnd()： 尾部补全
+
+'x'.padStart(5, 'ab') // 'ababx'
+'x'.padStart(4, 'ab') // 'abax'
+'x'.padEnd(5, 'ab') // 'xabab'
+'x'.padEnd(4, 'ab') // 'xaba'
+
+/** 04.---------- */
+//trimStart() or trimLeft() 
+//trimEnd() or trimRight()
+
+/**
+    数值
+    增加BigInt 基本类型
+ */
---
+++ es6/扩展-string-Array-Object.md
@@ -0,0 +1,24 @@
+### 字符串的扩展
+1. 模板字符串
+
+### 字符串新增方法
+> 得会读(你们跳过)
+实例方法:
+
++ includes()
++ startsWith()
++ endWith()
+
++ repeat
+    + replace(替换)有点像
++ padStart()
++ padEnd()
++ trimStart() or trimLeft()
++ tirmEnd() or trimRight()
+### 数值
+
+> BigInt 基本类型
+
+### 函数扩展
+
++ 默认参数
